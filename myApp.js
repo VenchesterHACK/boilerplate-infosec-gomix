458b92437e0f1c94db6ab314c7555a7a9ae54aff
VenchesterHACK <47230213+VenchesterHACK@users.noreply.github.com>
2021-11-04 16:06:48 +0300

    1

--- myApp.js
+++ myApp.js
@@ -1,6 +1,5 @@
-const express = require('экспресс');
+const express = require('express');
 const app = express();
-let helmet = require('helmet');
 
 
 
@@ -47,15 +46,16 @@ let helmet = require('helmet');
 
 
 
-export default app;
-import api from './server.js';
-app.use(static('public'));
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function(request, response) {
-    response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Your app is listening on port ${port}`);
+  console.log(`Your app is listening on port ${port}`);
 });
