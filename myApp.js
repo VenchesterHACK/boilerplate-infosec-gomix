791c3399f4ec05b8dbd942839ab8b2985853a079
VenchesterHACK <47230213+VenchesterHACK@users.noreply.github.com>
2021-11-04 15:20:10 +0300

    3

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 
@@ -47,15 +47,15 @@ const app = express();
 
 
 
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
+export default app;
+import api from './server.js';
+app.use(static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+    console.log(`Your app is listening on port ${port}`);
 });
